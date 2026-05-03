d9d5e811736dd6e301049634b1721b734276eb2a
amarebirhane <amarebirhane89@gmail.com>
2026-05-03 09:59:20 +0300

    updatd some features in the frontend for images

--- data/testimonial.js
+++ data/testimonial.js
@@ -2,24 +2,24 @@ export const testimonial = [
   {
     quote:
       "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
-    author: "Sarah Chen",
-    image: "https://randomuser.me/api/portraits/women/74.jpg",
+    author: "Selamawit GebreEgziabher",
+    image: "/testimonials/t1.png",
     role: "AI Engineer",
     company: "Tech Giant Co.",
   },
   {
     quote:
       "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
-    author: "Michael Rodriguez",
-    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    author: "Birhane Abrham",
+    image: "/testimonials/t2.png",
     role: "Product Manager",
     company: "StartUp Inc.",
   },
   {
     quote:
       "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
-    author: "Priya Patel",
-    image: "https://randomuser.me/api/portraits/women/75.jpg",
+    author: "Eden Tadesse",
+    image: "/testimonials/t3.png",
     role: "Marketing Director",
     company: "Global Corp",
   },
